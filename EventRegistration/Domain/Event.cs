4513fca40996afea96ec5aa9f677953b2f861471
MIN KHANT KYAW 2403900J <2403900J@student.tp.edu.sg>
2026-01-08 17:45:42 +0800

    Added entities

---
+++ EventRegistration/Domain/Event.cs
@@ -0,0 +1,12 @@
+﻿namespace EventRegistration.Domain
+{
+    public class Event : BaseDomainModel
+    {
+        public string? Title { get; set; }
+        public string? Type { get; set; }
+        public DateTime DateTime { get; set; }
+        public string? Location { get; set; }
+        public int TotalCapacity { get; set; }
+
+    }
+}
---
+++ EventRegistration/Domain/Registration.cs
@@ -0,0 +1,11 @@
+﻿namespace EventRegistration.Domain
+{
+    public class Registration : BaseDomainModel
+    {
+        public DateTime RegistrationDate { get; set; }
+
+        // Foreign Keys
+        public int EventId { get; set; }
+        public int TicketId { get; set; }
+    }
+}
