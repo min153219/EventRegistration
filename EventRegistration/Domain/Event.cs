515d304c6cbb01cf11ddee3e545c5c9a484ea903
MIN KHANT KYAW 2403900J <2403900J@student.tp.edu.sg>
2026-01-15 00:18:30 +0800

    ExploreEvents by category added

--- EventRegistration/Domain/Event.cs
+++ EventRegistration/Domain/Event.cs
@@ -7,5 +7,11 @@
         public DateTime EventDate { get; set; } 
         public string? Location { get; set; }
         public int TotalCapacity { get; set; }
+        public string? Category { get; set; }          
+        public string? Description { get; set; }       
+        public string? ImageUrl { get; set; }         
+        public bool IsFeatured { get; set; }
+
+        public virtual ICollection<Ticket>? Tickets { get; set; }
     }
 }
