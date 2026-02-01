e0b4b8d0e8c4f51024a3d4bb0f6a3424b19ae96a
MIN KHANT KYAW 2403900J <2403900J@student.tp.edu.sg>
2026-02-01 23:04:47 +0800

    updated paymentsystem

---
+++ EventRegistration/wwwroot/js/site.js
@@ -0,0 +1,9 @@
+﻿// CSV Download Function for Event Payments Export
+window.downloadFile = function (filename, base64Data) {
+    const link = document.createElement('a');
+    link.href = 'data:text/csv;base64,' + base64Data;
+    link.download = filename;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+};
